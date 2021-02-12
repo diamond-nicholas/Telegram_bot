c8cdc1f05e9f4faa68018d7a5e2e86ce4404cddb
diamond-nicholas <diamondnicholas154@gmail.com>
2021-02-12 17:11:06 +0100

    set the start command for the bot

--- lib/bot.rb
+++ lib/bot.rb
@@ -8,7 +8,7 @@ class Nicholas
       bot.listen do |message|
         case message.text
         when /start/i
-         bot.api.send_message(chat_id: message.chat.id, text: "hey nich you figured it out")
+         bot.api.send_message(chat_id: message.chat.id, text: "Hey #{message.from.first_name}, I'm a bot. I will make a list of websites to learn coding. try the /links command ")
         end
       end
     end
