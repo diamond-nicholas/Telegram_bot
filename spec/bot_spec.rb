071913d5d8e81dcf9e1caf3883d1f92b2e9f10a5
diamond-nicholas <diamondnicholas154@gmail.com>
2021-02-14 11:45:10 +0100

    created a new file to accept other methods

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,14 +1,29 @@
 require_relative '../lib/bot'
-
+require_relative '../lib/links'
 describe Klaus154 do
+  subject { Klaus154 }
+
   describe '#initialize' do
-    it 'returns a string of text of instructions when the start command is initiated' do
+    #     expect_any_instance_of(Klaus154).to receive(String).with("Hey #{message.from.first_name}, I'm a bot.
+    #  I will make a list of websites to learn coding. Try the /link command. For more links use the /more command ")
+    #      Klaus154.new('/start/i').initialize
+
+    it 'returns a Nilclass' do
       expect(initialize.class).to eql(NilClass)
     end
 
     it 'returns a string of text of instructions when the start command is initiated' do
-      expect(initialize.(/start/i)).to eql("Hey #{message.from.first_name}, I'm a bot.
- I will make a list of websites to learn coding. Try the /link command. For more links use the /more command ")
+      sentence = "Hey nich, I'm a bot.
+ I will make a list of websites to learn coding. Try the /link command. For more links use the /more command "
+      expect(initialize.call(/start/i)).to eql(sentence)
+    end
+  end
+end
+
+describe Links do
+  describe 'web_link' do
+    it 'returns an array' do
+      expect(Links.new.web_link).to be_a Array
     end
   end
 end
